cfb153e3c77f8873c94ce86c25e945c9a5c07a2b
Chin Huang <chinhuang1005@gmail.com>
2018-08-10 15:55:03 +0800

    add US

--- driving.py
+++ driving.py
@@ -7,3 +7,8 @@ if country == '台灣' or country == '臺灣' or country == 'Taiwan':
 		print('你可以考駕照')
 	else:
 		print('你還不可以考駕照')
+elif country == '美國':
+	if age >= 16:
+		print('你可以考駕照')
+	else:
+		print('你還不可以考駕照')
